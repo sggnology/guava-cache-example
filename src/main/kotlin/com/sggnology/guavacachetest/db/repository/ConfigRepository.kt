002fc2c543451b971210e183ab3a644b356cfefe
sggnology <sggnology@gmail.com>
2023-08-25 22:31:16 +0900

    [service] db > config, 모든 key 값을 반환하는 query 구현

--- src/main/kotlin/com/sggnology/guavacachetest/db/repository/ConfigRepository.kt
+++ src/main/kotlin/com/sggnology/guavacachetest/db/repository/ConfigRepository.kt
@@ -3,7 +3,13 @@ package com.sggnology.guavacachetest.db.repository
 
 import com.sggnology.guavacachetest.db.entity.Config
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface ConfigRepository: JpaRepository<Config, Long> {
     fun findByKey(key: String): Config?
+
+    @Query("""
+        select c.key from Config c
+    """)
+    fun findAllKeys(): List<String>
 }
