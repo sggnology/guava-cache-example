74657c29a8b71f22955f2a4d34003d96532ca9fe
LuLuLaLaLa <haesung0728@gmail.com>
2023-08-25 20:19:30 +0900

    [init] 프로젝트 구성

---
+++ src/main/kotlin/com/sggnology/guavacachetest/GuavaCacheTestApplication.kt
@@ -0,0 +1,11 @@
+package com.sggnology.guavacachetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuavaCacheTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<GuavaCacheTestApplication>(*args)
+}
